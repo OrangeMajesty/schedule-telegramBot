edae657ebed6aa362006c16209c5c51d6966736d
Dmitry <dmitri.kulakov.000@gmail.com>
2019-05-03 10:31:11 +0500

    Исправленны вылеты соединения с БД

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var telegram = require('telegram-bot-api');
 var token = require('./token');
+var db = require('./db.js');
 
 var api = new telegram({
     token: token.id,
@@ -9,24 +10,141 @@ var api = new telegram({
     }
 });
 
+var startButton = {
+    inline_keyboard: [
+        [
+            {
+                text: 'Выбор группы',
+                callback_data: 'bind'
+            }
+        ],
+        [
+            {
+                text: 'Настройки',
+                callback_data: 'config'
+            }
+        ],
+        [
+            {
+                text: 'Помощь',
+                callback_data: 'help'
+            }
+        ]
+    ]
+};
+
+
+
 api.on('message', function(message)
 {
     var chat_id = message.chat.id;
 
-    // It'd be good to check received message type here
-    // And react accordingly
-    // We consider that only text messages can be received here
-
     api.sendMessage({
         chat_id: message.chat.id,
-        text: message.text ? message.text : 'This message doesn\'t contain text :('
-    })
-        .then(function(message)
-        {
-            console.log(message);
-        })
-        .catch(function(err)
-        {
-            console.log(err);
-        });
+        text: "Вас приветствует Актюбинский Высший политехнический колледж\n\nВыберите команду.",
+        reply_markup: JSON.stringify(startButton)
+    });
+
 });
+
+api.on('inline.callback.query', function(rep) {
+
+    let _case = rep.data.split(':')[0];
+    let _data = rep.data.split(':')[1];
+
+    console.log(_case);
+
+    switch(_case) {
+        case 'config': config(rep.message.chat); break;
+
+        case 'bind': bind(rep.message.chat, 0); break;
+        case 'bind_Deps': bind(rep.message.chat, 1, _data); break;
+        case 'bind_Groups': bind(rep.message.chat, 2, _data); break;
+
+        default: help(rep.message.chat);
+    }
+
+});
+
+async function config(user) {
+    api.sendMessage({
+        chat_id: user.id,
+        text: "."
+    });
+}
+
+async function help(user) {
+    api.sendMessage({
+        chat_id: user.id,
+        text: "."
+    });
+}
+
+async function bind(user, level, data = null) {
+    switch(level) {
+        case 2:
+            console.log(user);
+            api.sendMessage({
+                chat_id: user.id,
+                text: "Вы успешно привязаны к группе."
+            });
+            break;
+        case 1:
+            console.log(data);
+            let groups = await db.getGroupsById(data);
+
+            var genButton = new Object();
+
+            genButton.inline_keyboard = new Array();
+            genButton.inline_keyboard.push(new Array());
+            let col = 0;
+
+            for (var groupsIndex = 0; groupsIndex != groups.length; groupsIndex++) {
+
+                var tempButton = new Object();
+
+                tempButton.text = groups[groupsIndex].name;
+                tempButton.callback_data = 'bind_' + groups[groupsIndex].class + ':' + groups[groupsIndex].name;
+
+                if(groupsIndex%4 == 0) {
+                    genButton.inline_keyboard.push(new Array());
+                    col++;
+                }
+
+                genButton.inline_keyboard[col].push(tempButton);
+            }
+
+            api.sendMessage({
+                chat_id: user.id,
+                text: "Выберите группу",
+                reply_markup: JSON.stringify(genButton)
+            });
+            break;
+        case 0:
+            console.log(user);
+            let deps = await db.getDeps();
+
+            var genButton = new Object();
+
+            genButton.inline_keyboard = new Array();
+
+            for (var depsIndex = 0; depsIndex != deps.length; depsIndex++) {
+
+                var tempButton = new Object();
+                tempButton.text = deps[depsIndex].name;
+
+                tempButton.callback_data = 'bind_' + deps[depsIndex].class + ':' + deps[depsIndex].id;
+                genButton.inline_keyboard.push(new Array());
+                genButton.inline_keyboard[depsIndex].push(tempButton);
+            }
+
+            api.sendMessage({
+                chat_id: user.id,
+                text: "Выберите отделение",
+                reply_markup: JSON.stringify(genButton)
+            });
+
+            break;
+    }
+
+}
---
+++ db.js
@@ -0,0 +1,119 @@
+var mysql = require('mysql');
+
+var con = mysql.createPool({
+    host    : "localhost",
+    user    : "root",
+    port    : '3307',
+    password: "root1234",
+    database: 'ahpc-edu'
+});
+
+
+
+// function handleDisconnect() {
+//     // con.connect(function(err) {
+//     //     if(err) {
+//     //         console.log('error when connecting to db:', err);
+//     //         setTimeout(handleDisconnect, 2000);
+//     //     }
+//     // });
+//
+//     // con.on('error', function(err) {
+//     //     console.log('db error', err);
+//     //     if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+//     //         handleDisconnect();
+//     //     } else {
+//     //         throw err;
+//     //     }
+//     // });
+// }
+
+module.exports.getDeps = function () {
+    return new Promise(function(resolve) {
+        var deps = new Array();
+
+        con.getConnection(function(err, connect) {
+            con.query('SELECT * FROM tb_department', function(error, result, fields){
+
+                if (err) throw error;
+
+                if(result)
+                    for (var i = 0; i != result.length; i++){
+
+                        let i_dep = new Object();
+
+                        i_dep.id = result[i].id_department;
+                        i_dep.name = result[i].name_department;
+                        i_dep.class = 'Deps';
+
+                        deps.push(i_dep);
+
+                    }
+                connect.release();
+                resolve(deps);
+            });
+
+        });
+
+
+    });
+};
+
+module.exports.getGroups = function () {
+    return new Promise(function(resolve) {
+        var deps = new Array();
+
+        con.getConnection(function(err, connect) {
+            con.query('SELECT * FROM tb_group', function (error, result, fields) {
+
+                if (err) throw error;
+
+                if (result)
+                    for (var i = 0; i != result.length; i++) {
+
+                        let i_dep = new Object();
+
+                        i_dep.id = result[i].id_group;
+                        i_dep.name = result[i].name_group;
+                        i_dep.idDep = result[i].id_department;
+                        i_dep.class = 'Groups';
+
+                        deps.push(i_dep);
+                    }
+                connect.release();
+                resolve(deps);
+            });
+
+        });
+    });
+};
+
+module.exports.getGroupsById = function (id) {
+    return new Promise(function(resolve) {
+        var deps = new Array();
+
+        con.getConnection(function(err, connect) {
+            con.query('SELECT * FROM tb_group WHERE `id_department` = ?', id, function (error, result, fields) {
+
+                if (err) throw error;
+
+                if (result)
+                    for (var i = 0; i != result.length; i++) {
+
+                        let i_dep = new Object();
+
+                        i_dep.id = result[i].id_group;
+                        i_dep.name = result[i].name_group;
+                        i_dep.idDep = result[i].id_department;
+                        i_dep.class = 'Groups';
+
+                        deps.push(i_dep);
+                    }
+                connect.release();
+                resolve(deps);
+            });
+        });
+
+    });
+};
+
